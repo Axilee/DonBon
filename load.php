b76b2a86d4035db6651d74fe6902b294492577fd
barcinx <gruba.impra.xd@gmail.com>
2023-03-07 19:51:28 +0100

    load.php zmiana z explode na preg_split lepsze zachowanie przy dzieleniu stringów main.js za duzo zmian zeby opisac dziala funkcjonalnosc odczytu checkboxów tylko value do wypelnienia

--- load.php
+++ load.php
@@ -1,14 +1,12 @@
 <?php
 if ($file = fopen("data/data_save.ini","r")){
-
     fgets($file);
-
     while(!feof($file)){
         $textperline = fgets($file);
-        $data = explode("=", $textperline);
+        $data = preg_split("/\s*=\s*/", $textperline);
         $key = trim($data[0]);
         $value = trim($data[1]); 
-        echo("$key: $value<br>");
+        echo("$key:$value<br>");
     }
     fclose($file);
 }
