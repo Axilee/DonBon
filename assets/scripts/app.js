d7109911efb7c8b42d674a9f7d526ca25b726036
xMonUx <infinityfor0m@gmail.com>
2023-03-08 18:20:04 +0100

    Wersja 4.2 poprawna bez cursor effect

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,3 @@
-
-window.onpointermove = event => { 
-  const { clientX, clientY } = event;
-  const cursorEfect = document.getElementById("cursorEfect");
-
-
-  cursorEfect.animate({
-    left: `${clientX}px`,
-    top: `${clientY}px`
-  }, { duration: 3000, fill: "forwards" });
-}
-
 window.addEventListener('load',function(){
   document.querySelector('body').classList.add("loaded")  
 });
