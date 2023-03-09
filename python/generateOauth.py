8add075ba33a84d9d757ac82487db590b2ac5682
Aaxile <kacper3fitas@gmail.com>
2023-03-09 02:15:35 +0100

    webhook pobiera poprawnie kod/zrobic post do uzyskania tokenu kodem

---
+++ python/generateOauth.py
@@ -0,0 +1,28 @@
+import webbrowser
+#from main import ACCESS_TOKEN
+#do zmiany na publiczny adres redirect po autoryzacji, tam wysyla sie token oauth2 (narazie jest w linku po prostu XD)
+redirect_uri="http://localhost:5000"
+
+
+
+client_id="nohxc0resfams2ui1ftvvs07awax2c"
+link = "https://id.twitch.tv/oauth2/"
+client_uri = f"authorize?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&scope="
+scope=""
+with open("permisjeDoLinku") as permisje:
+    linie = permisje.readlines()
+    for l in linie:
+        l = l.replace(":","%3A")
+        scope = scope.__add__(l)
+        scope = scope.strip()
+        scope = scope.__add__("+")
+    scope = scope.removesuffix("+")
+                
+link = link+client_uri+scope+"&state=c3ab8aa609ea11e793ae92361f002671"
+print(link)
+webbrowser.open(link)
+
+
+
+#https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=nohxc0resfams2ui1ftvvs07awax2c&redirect_uri=http://localhost&scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls+bits%3Aread&state=c3ab8aa609ea11e793ae92361f002671
+
--- python/webhook.py
+++ python/webhook.py
@@ -2,13 +2,17 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def handle_webhook():
     print("e")
-    data = request.get_json()
-    print (data)
+    # data = request.get_json()
+    code = request.args.get('code') #pobiera ten jebany kod
+    scope = request.args.getlist('scope') #pobiera scope permisji //lista
+    
+    
+    print (f"Scope={scope}\n\nKod = {code}\n\n")
     # process the data received from the webhook
-    return 'OK'
+    return f'<h1><b>scope = {scope}<br>kod = {code}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
