0be0e2324882e1ba0fceaf585c8dcd26f3ea7e76
barcinx <gruba.impra.xd@gmail.com>
2023-03-11 17:10:29 +0100

    linux to cipa

---
+++ python/test.py
@@ -0,0 +1,13 @@
+import tkinter
+from Oauth2 import webhook
+from Oauth2 import AuthorizationOauth2 
+from Oauth2 import initWebhook
+import asyncio
+
+
+async def test():
+    print('chuj')
+async def main():
+    await initWebhook.inicjalizuj.wybor()
+
+asyncio.run(main())
