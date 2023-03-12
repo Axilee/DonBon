86c940ccf31b46985a890b2bd65e65f50b3126d7
Aaxile <kacper3fitas@gmail.com>
2023-03-12 16:05:55 +0100

    cudzyslowia do data_save.ini

--- load.php
+++ load.php
@@ -1,12 +1,6 @@
 <?php
-$ini = file('assets\data\data_save.ini', FILE_IGNORE_NEW_LINES);
-$ini = array_slice($ini, 1);
-$ini_array = [];
 
-foreach($ini as $element){
-    $element_temp = explode(' = ', $element);
-    $ini_array[$element_temp[0]] = $element_temp[1];
-}
-header('Content-Type: application/json');
+$ini_array = parse_ini_file("data/data_save.ini",true);
+// header('Content-Type: application/json');
 echo json_encode($ini_array);
 ?>
