24014fea54a35bc831cbe5e42bc678a1d75ffcd2
xMonUx <infinityfor0m@gmail.com>
2023-03-18 20:19:04 +0100

    W 100% naprawione stronnicowanie

--- strona/scripts/app.js
+++ strona/scripts/app.js
@@ -91,6 +91,7 @@ window.addEventListener('load',function(){
     });
   });
 
+//Notyfikacje
   $(document).ready(function() {
     $('.save-settings').click(function() {
       $('.Message').addClass('is-hidden');
@@ -100,30 +101,6 @@ window.addEventListener('load',function(){
         setTimeout(function() {
           $('.Message--green').addClass('is-hidden');
           $('.Message--green').removeClass('hidding');
-        }, 400);
-      }, 5000);
-    });
-  });
-
-//Notyfikacje
-  $(document).ready(function() {
-    var clickCount = 0;
-  
-    $('.save-settings').click(function() {
-      clickCount++;
-  
-      if (clickCount >= 5) {
-        $('.Message').removeClass('Message--green').addClass('Message--red');
-      }
-  
-      $('.Message').removeClass('is-hidden');
-  
-      setTimeout(function() {
-        $('.Message').addClass('flyOut');
-        setTimeout(function() {
-          $('.Message').addClass('is-hidden').removeClass('flyOut');
-          $('.Message').removeClass('Message--red').addClass('Message--green');
-          clickCount = 0;
         }, 400);
       }, 5000);
     });
@@ -139,73 +116,70 @@ window.addEventListener('load',function(){
   
 
 //Stronnicowanie
-  var pr = document.querySelector('.paginate.left');
-  var pl = document.querySelector('.paginate.right');
+  var pr = $('.paginate.left');
+  var pl = $('.paginate.right');
   var pageNames = ['DonBon', 'AxBon', 'BierBon', 'JarBon', 'BauBon']; // tablica nazw stron
 
-  pr.onclick = slide.bind(this, -1);
-  pl.onclick = slide.bind(this, 1);
+  pr.click(function() { slide(-1) });
+  pl.click(function() { slide(1) });
 
-  var index = 0,
-    total = 5; //Liczba stron
+  var index = 0, total = 5; //Liczba stron
 
   var startX = null;
   var dragging = false;
 
   function slide(offset) {
     index = Math.min(Math.max(index + offset, 0), total - 1);
-  
+
     function getPages(index) {
       var prevPage = index > 0 ? pageNames[index - 1] : null;
       var nextPage = index < pageNames.length - 1 ? pageNames[index + 1] : null;
       var currentPage = pageNames[index];
-      return [prevPage, currentPage, nextPage];
+    return [prevPage, currentPage, nextPage];
     }
-  
+
     var [prevPage, currentPage, nextPage] = getPages(index);
-  
-    document.querySelector('.prev-page').textContent = prevPage || '';
-    document.querySelector('.current-page').textContent = currentPage || '';
-    document.querySelector('.next-page').textContent = nextPage || '';
-  
-    pr.setAttribute('data-state', index === 0 ? 'disabled' : '');
-    pl.setAttribute('data-state', index === total - 1 ? 'disabled' : '');
-  
-    // Dodaj kod, który będzie wyświetlał odpowiedni kontener dla danej strony
+
+    $('.prev-page').text(prevPage || '');
+    $('.current-page').text(currentPage || '');
+    $('.next-page').text(nextPage || '');
+
+    pr.attr('data-state', index === 0 ? 'disabled' : '');
+    pl.attr('data-state', index === total - 1 ? 'disabled' : '');
+
     if (currentPage === 'DonBon') {
-      document.querySelector('.app-container').style.display = 'block';
-      document.querySelector('.app-container2').style.display = 'none';
+      $('.app-container').css('display', 'block');
+      $('.app-container2').css('display', 'none');
     } else if (currentPage === 'AxBon') {
-      document.querySelector('.app-container').style.display = 'none';
-      document.querySelector('.app-container2').style.display = 'block';
-    } 
+      $('.app-container').css('display', 'none');
+      $('.app-container2').css('display', 'block');
+    }
   }
-  
 
   slide(0);
 
-  var counter = document.querySelector('.counter');
+  var counter = $('.counter');
 
-  counter.addEventListener('mousedown', function(event) {
+    counter.mousedown(function(event) {
     startX = event.pageX;
     dragging = true;
   });
 
-  document.addEventListener('mousemove', function(event) {
+  $(document).mousemove(function(event) {
     if (dragging) {
       var diffX = event.pageX - startX;
-      if (Math.abs(diffX) > 50) { // przesuwanie jest większa różnica niż 50px
-        slide(diffX > 0 ? -1 : 1); // jeśli przesuwamy w prawo (diffX > 0), to zmniejszamy index o 1, w przeciwnym przypadku zwiększamy o 1
-        startX = event.pageX;
+    if (Math.abs(diffX) > 50) { // przesuwanie jeżeli jest większa różnica niż 50px
+      slide(diffX > 0 ? -1 : 1); // jeśli przesuwamy w prawo (diffX > 0), to zmniejszamy index o 1, w przeciwnym przypadku zwiększamy o 1
+      startX = event.pageX;
       }
     }
   });
 
-  document.addEventListener('mouseup', function(event) {
-  dragging = false;
+  $(document).mouseup(function(event) {
+    dragging = false;
   });
 
-
+  
 
 
 
