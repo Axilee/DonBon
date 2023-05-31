60c1af0b40c0a1e72ab3803bd051cff097b422f3
barcinx <barcinx@users.noreply.github.com>
2023-05-31 19:17:16 +0200

    dodanie mozliwosci wprowadzania cen

--- strona/scripts/main.js
+++ strona/scripts/main.js
@@ -4,67 +4,84 @@ $.get("load.php", function(data) {
   console.log(data);
   $.each(data.KOMENDY, function(key, value){
     if(value == 1){
-      var checkbox = $("<input>").addClass("switch switch-data").attr({
+      var checkbox = $("<input>").addClass("switch switch-data zmienne").attr({
         type: "checkbox",
         value: key,
         checked : true
       });
       var label = $("<label>").attr("for", "s1").text(key);
-      var div = $("<div>").append(checkbox, label).attr({id: 'KOMENDY'});
+      var div = $("<div>").append(checkbox, label).attr({id: 'KOMENDY', 'data-command': key });
       $("#komendy-checkboxes").append(div);      
     }else{
-      var checkbox = $("<input>").addClass("switch switch-data").attr({
+      var checkbox = $("<input>").addClass("switch switch-data zmienne").attr({
         type: "checkbox",
         value: key
       });
       var label = $("<label>").attr("for", "s1").text(key);
-      var div = $("<div>").append(checkbox, label).attr({id: 'KOMENDY'});
+      var div = $("<div>").append(checkbox, label).attr({id: 'KOMENDY', 'data-command': key });
       $("#komendy-checkboxes").append(div);      
     }
-    var nDiv = $("<div>", {id: "kurwa"});
-    nDiv.appendTo("#KOMENDY");
-
   });
   $.each(data.BITSY, function(key, value){
     if(value == 1){
-      var checkbox = $("<input>").addClass("switch switch-data").attr({
+      var checkbox = $("<input>").addClass("switch switch-data zmienne").attr({
         type: "checkbox",
         value: key,
         checked : true
       });
       var label = $("<label>").attr("for", "s1").text(key);
-      var div = $("<div>").append(checkbox, label).attr({id: 'BITSY'});
+      var div = $("<div>").append(checkbox, label).attr({id: 'BITSY', 'data-command': 'BITSY'+key });
       $("#bitsy-checkboxes").append(div);      
     }else{
-      var checkbox = $("<input>").addClass("switch switch-data").attr({
+      var checkbox = $("<input>").addClass("switch switch-data zmienne").attr({
         type: "checkbox",
         value: key
       });
       var label = $("<label>").attr("for", "s1").text(key);
-      var div = $("<div>").append(checkbox, label).attr({id: 'BITSY'});
+      var div = $("<div>").append(checkbox, label).attr({id: 'BITSY', 'data-command': 'BITSY'+key });
       $("#bitsy-checkboxes").append(div);      
     }
 
   });
   $.each(data.POINTSY, function(key, value){
     if(value == 1){
-      var checkbox = $("<input>").addClass("switch switch-data").attr({
+      var checkbox = $("<input>").addClass("switch switch-data zmienne").attr({
         type: "checkbox",
         value: key,
         checked : true
       });
       var label = $("<label>").attr("for", "s1").text(key);
-      var div = $("<div>").append(checkbox, label).attr({id: 'POINTSY'});
+      var div = $("<div>").append(checkbox, label).attr({id: 'POINTSY', 'data-command': 'POINTSY'+key });
       $("#pointsy-checkboxes").append(div);      
     }else{
-      var checkbox = $("<input>").addClass("switch switch-data").attr({
+      var checkbox = $("<input>").addClass("switch switch-data zmienne").attr({
         type: "checkbox",
         value: key
       });
       var label = $("<label>").attr("for", "s1").text(key);
-      var div = $("<div>").append(checkbox, label).attr({id: 'POINTSY'});
+      var div = $("<div>").append(checkbox, label).attr({id: 'POINTSY', 'data-command': 'POINTSY'+key });
       $("#pointsy-checkboxes").append(div);      
     }
+  });
+  $.each(data.VALBITSY, function(key, value){
+    $('div[data-command="BITSY' + key +'"]').each(function() {
+    var valueBox = $("<input>").addClass("input-data zmienne").attr({
+      type: "number",
+      value: value,
+      name: key
+    });
+      $(this).append(valueBox);
+    });
+  });
+  $.each(data.VALPOINTSY, function(key, value){
+    $('div[data-command="POINTSY' + key +'"]').each(function() {
+    var valueBox = $("<input>").addClass("input-data zmienne").attr({
+      type: "number",
+      value: value,
+      name: key
+    });
+      $(this).append(valueBox);
+    });
   });
 
   // $.each(data.VALKOMENDY, function(key, value){
@@ -89,12 +106,12 @@ $.get("load.php", function(data) {
   //   $(".ch-points").append(valueBox);
   // });
 
-$('.switch-data').click(function() {
+$('.zmienne').on('click keydown',function(event) {
+  if(event.type === 'click' || (event.type === 'keydown' && event.keyCode === 13)) {
   const checkboxObj = {
     KOMENDY: [],
     BITSY: [],
     POINTSY: [],
-    VALKOMENDY: [],
     VALBITSY: [],
     VALPOINTSY: []
   }
@@ -106,13 +123,17 @@ $('.switch-data').click(function() {
       checkboxObj[prefix].push(this.value + " = 0");
     }
     });
+  $('.input-data').each(function () {
+    const prefix = $(this).parent().attr('id');
+      checkboxObj['VAL'+prefix].push(this.name + " = " + this.value);
+  });
     const checkboxObjJson = JSON.stringify(checkboxObj)
     console.log(checkboxObjJson)
     dataToWrite = {
       content: checkboxObjJson
     }
     $.post("save.php", dataToWrite)
-
+  }
 });
 
 });
